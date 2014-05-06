86d443f34b93f59dc7a4ed7e51f46a42b2c79eb1
Ben Dixon <ben@hillsbede.co.uk>
2014-05-06 22:43:30 +0100

    bumps version following nodejs addition

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "rails_gem_dependencies-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@hillsbede.co.uk"
 description       "Installs packages commonly required by rails gems"
-version           "0.0.1"
+version           "0.0.2"
 
 recipe "rails_gem_dependencies-tlq", "all the gems"
 
