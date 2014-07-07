f62ff22492dd9a2c31cd856de176c19f0fbe2d73
Ben Dixon <ben@hillsbede.co.uk>
2014-07-07 12:21:34 +0100

    Add dependency on the apt cookbook

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,10 @@ name              "rails_gem_dependencies-tlq"
 maintainer        "Ben Dixon"
 maintainer_email  "ben@hillsbede.co.uk"
 description       "Installs packages commonly required by rails gems"
-version           "0.0.2"
+version           "0.0.3"
 
 recipe "rails_gem_dependencies-tlq", "all the gems"
 
 supports "ubuntu"
+
+depends "apt"
