e90bfea798e93e64ca7bcd36527ccbf4f8d55a60
Ben <ben@talkingquickly.co.uk>
2021-03-06 07:15:55 +0000

    adds: repos for yarn and node

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,20 @@
+apt_repository 'yarn' do
+  uri 'https://dl.yarnpkg.com/debian/'
+  key 'https://dl.yarnpkg.com/debian/pubkey.gpg'
+  components ['main']
+  distribution 'stable'
+  action :add
+end
+
+apt_repository 'node' do
+  uri 'https://deb.nodesource.com/node_14.x'
+  key 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key'
+  components ['main']
+  action :add
+end
+
+apt_update 'update'
+
 package 'build-essential'
 package 'autoconf'
 package 'libtool'
@@ -11,4 +28,6 @@ package 'imagemagick'
 package 'libmagickwand-dev'
 package 'curl'
 package 'libcurl4'
-package 'libcurl4-dev'
+package 'libcurl4-gnutls-dev'
+package 'libcurl4-nss-dev'
+package 'libcurl4-openssl-dev'
